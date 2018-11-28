5c086d6f3a15218b6a4e60994be1f8fafd8001a3
Carlos Gomes <gomescarlos93ale@gmail.com>
2018-11-27 22:59:57 -0200

    Ajustes contra SQL_INJECT

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -16,15 +16,15 @@ class TaskController extends Controller
      */
     public function getTaskById($id)
     {
-		$data['tarefa']   = Task::find($id);
+        $data['tarefa']   = Task::find($id);
 
         $response = array(
-       		'data' 		=> $data,
+            'data'      => $data,
             'status'    => 'success',
-            'msg'    	=> 'Registros retornado com sucesso.',
-        );      		
-		return response()->json($response);   
-    }	
+            'msg'       => 'Registros retornado com sucesso.',
+        );              
+        return response()->json($response);   
+    }   
 
     /**
      * Funçao responsavel por buscar todas as tarefas
@@ -34,34 +34,34 @@ class TaskController extends Controller
     public function getTasks()
     {
 
-    	try {
-    		$data['tarefas']['aguardando']   = Task::whereNull('data_remocao')->where('task_status_id','1')->get();
-    		$data['tarefas']['em_andamento'] = Task::whereNull('data_remocao')->where('task_status_id','2')->get();
-    		$data['tarefas']['pausada'] 	 = Task::whereNull('data_remocao')->where('task_status_id','3')->get();
-    		$data['tarefas']['concluidas'] 	 = Task::whereNull('data_remocao')->where('task_status_id','4')->get();
+        try {
+            $data['tarefas']['aguardando']   = Task::whereNull('data_remocao')->where('task_status_id','1')->get();
+            $data['tarefas']['em_andamento'] = Task::whereNull('data_remocao')->where('task_status_id','2')->get();
+            $data['tarefas']['pausada']      = Task::whereNull('data_remocao')->where('task_status_id','3')->get();
+            $data['tarefas']['concluidas']   = Task::whereNull('data_remocao')->where('task_status_id','4')->get();
 
-	        if(count($data['tarefas']) > 0 ){
-		        $response = array(
-		       		'data' 		=> $data,
-		            'status'    => 'success',
-		            'msg'    	=> 'Registros retornado com sucesso.',
-		        );
-	        }else{
-		        $response = array(
-		       		'data' 		=> '',
-		            'status'    => 'warning',
-		            'msg'    	=> 'Não foi possível localizar nenhuma tarefa no momento.',
-		        );        	
-	        }    		
-    	} catch (Exception $e) {
-		        $response = array(
-		       		'data' 		=> '',
-		            'status'    => 'error',
-		            'msg'    	=> 'Algo deu errado tente novamente.',
-		        );      		
-    	}
-		
-		return response()->json($response); 
+            if(count($data['tarefas']) > 0 ){
+                $response = array(
+                    'data'      => $data,
+                    'status'    => 'success',
+                    'msg'       => 'Registros retornado com sucesso.',
+                );
+            }else{
+                $response = array(
+                    'data'      => '',
+                    'status'    => 'warning',
+                    'msg'       => 'Não foi possível localizar nenhuma tarefa no momento.',
+                );          
+            }           
+        } catch (Exception $e) {
+                $response = array(
+                    'data'      => '',
+                    'status'    => 'error',
+                    'msg'       => 'Algo deu errado tente novamente.',
+                );              
+        }
+        
+        return response()->json($response); 
 
     }
 
@@ -72,12 +72,12 @@ class TaskController extends Controller
      */
     public function salvar()
     {
-    	$dados = $_POST;
+        $dados = $_POST;
 
         if (empty($dados['id'])) {
             $task                      = new Task();
-            $task->titulo   		   = $dados['titulo'];
-            $task->descricao   		   = $dados['descricao'];
+            $task->titulo              = $this->removerCaracteresEspeciais(strip_tags($dados['titulo']));
+            $task->descricao           = $this->removerCaracteresEspeciais(strip_tags($dados['descricao']));
             $task->task_status_id      = 1;
             $task->save();  
             $response = array(
@@ -86,9 +86,9 @@ class TaskController extends Controller
             );
         }else{
             $task                      = Task::find($dados['id']);
-            $task->titulo   		   = $dados['titulo'];
-            $task->descricao   		   = $dados['descricao'];
-            $task->data_edicao   	   = DB::raw('CURRENT_TIMESTAMP(0)');
+            $task->titulo              = $this->removerCaracteresEspeciais(strip_tags($dados['titulo']));
+            $task->descricao           = $this->removerCaracteresEspeciais(strip_tags($dados['descricao']));
+            $task->data_edicao         = DB::raw('CURRENT_TIMESTAMP(0)');
             $task->save();  
             $response = array(
                 'status'    => 'success',
@@ -104,8 +104,8 @@ class TaskController extends Controller
      * @return valide success
      */
     public function deletar($id){
-    	
-        $task         		  = Task::find($id);
+        
+        $task                 = Task::find($id);
         $task->data_remocao   = DB::raw('CURRENT_TIMESTAMP(0)');
 
         $task->save();
@@ -119,18 +119,40 @@ class TaskController extends Controller
      * @return valide success
      */
     public function alterar_status(){
-    	$dados = $_POST;
+        $dados = $_POST;
 
-        $task         			= Task::find($dados['id']);
+        $task                   = Task::find($dados['id']);
         if($dados['status'] == 4){
-        	$task->data_conclusao   	= DB::raw('CURRENT_TIMESTAMP(0)');
+            $task->data_conclusao       = DB::raw('CURRENT_TIMESTAMP(0)');
         }else{
-        	$task->data_conclusao   	= null;
+            $task->data_conclusao       = null;
         }
         $task->task_status_id   = $dados['status'];
-        $task->data_edicao   	= DB::raw('CURRENT_TIMESTAMP(0)');
+        $task->data_edicao      = DB::raw('CURRENT_TIMESTAMP(0)');
         $task->save();
 
         return response()->json(array('success' => true));
+    }
+
+
+
+    /**
+     * Funcao responsavel por remover caracteres especiais enviando SQL ou JavaScript no código.
+     *
+     * @return palavra
+     */
+    private function removerCaracteresEspeciais($str){
+        /*** Função para retornar uma string/Array protegidos contra SQL/Blind/XSS Injection*/
+        if( !is_array( $str ) ) {                      
+                $str = preg_replace( '/(case|while|alert|from|select|insert|delete|where|drop|union|order|update|database)/i', '', $str );
+                $str = preg_replace( '/(&lt;|<)?script(\/?(&gt;|>(.*))?)/i', '', $str );
+                $tbl = get_html_translation_table( HTML_ENTITIES );
+                $tbl = array_flip( $tbl );
+                $str = addslashes( $str );
+                $str = strip_tags( $str );
+                return strtr( $str, $tbl );
+        } else {
+                return array_filter( $str, "protect" );
+        }
     }
 }
